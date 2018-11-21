529b0131eeb275df82fc92f99f769e62b3ec311d
Edward Vidal Jr <develone@sbcglobal.net>
2018-11-21 10:27:45 -0700

    starting to work on programs needed to load and debug C programs in catzip

--- sw/host/buildsdramscope.sh
+++ sw/host/buildsdramscope.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-rm -f arm-sdramscope
+rm -f arm-sdramscope arm-zipload
 g++ -g -Wall -I. -I../../rtl -c scopecls.cpp -o obj-arm/scopecls.o
 g++ -g -Wall -I. -I../../rtl -c sdramscope.cpp -o obj-arm/sdramscope.o
+g++ -g -Wall -I. -I../../rtl -c zipload.cpp -o obj-arm/zipload.o
 g++ -g obj-arm/sdramscope.o obj-arm/scopecls.o obj-arm/hexbus.o obj-arm/llcomms.o obj-arm/regdefs.o obj-arm/byteswap.o -o arm-sdramscope

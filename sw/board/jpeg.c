724f8b3007a5b9d37f089153f38cf7f8411fe8d8
Edward Vidal Jr <develone@sbcglobal.net>
2018-11-26 08:44:53 -0700

    When adding "board.h" needed to modify the catzip/sw/board/Makefile This was since design.h was not being found.
    -       $(CC) -O3 -c  $< -o $@
    +       $(CC) $(CFLAGS) -c  $< -o $@
    These are not working
    
    *_gpio = GPIO_LEDR_SET;
    *_gpio = 0x010001;
    *_gpio = 0x010000;
    When uncommented the cpu does not run.
    . gpio 0x8
    . img_ptr = 0x200afc8
    . Start of JPEG DWT!
    . buf_ptr = 0x800000
    . m_ptr = 0x200c790
    . Clr m_ptr !
    . clearing buf_ptr + 786432 buf_ptr = 0x800000
    . done clearing buf_ptr + 786432
    . free m_ptr !
    . img_ptr = 0x200afcc

--- sw/board/jpeg.c
+++ sw/board/jpeg.c
@@ -1,7 +1,51 @@
+
 #include <stdio.h>
+#include <stdlib.h>
+#include "board.h"
 #include "lifting.h"
+#define SDRAM 0x2000000
+#define imgsize 256
+
+void zip_clear_sdram(int *imbuf) {
+int val,i;
+
+val = 0;
+ 
+ 
+ 
+for(i=0; i<256*256*12; i++) {
+	*imbuf++ = val;
+}
+}
+
 int main(int argc, char **argv) {
 	int *buf_ptr = (int *)0x800000;
+	int *img_ptr = (int *)(SDRAM + 45000);
+	int *m_ptr;
+	 
+	int val,i;
+	//*_gpio = GPIO_LEDR_SET;
+	printf("gpio 0x%x\n",_gpio);
+	printf("img_ptr = 0x%x\n",img_ptr);
+	//int img_mem = imgsize*imgsize*3;
+	//printf("img_mem = %ld\n",img_mem);
 	printf("Start of JPEG DWT!\n");
 	printf("buf_ptr = 0x%x\n",buf_ptr);
+	m_ptr = malloc(196608);
+	printf("m_ptr = 0x%x\n",m_ptr);
+	val = 0;
+	printf("Clr m_ptr !\n");
+	
+	//256*256*12 = 786432
+	//this was done by clr_sdram
+	printf("clearing buf_ptr + 786432 buf_ptr = 0x%x\n",buf_ptr);
+	zip_clear_sdram(buf_ptr);
+	printf("done clearing buf_ptr + 786432 \n");
+	printf("free m_ptr !\n");	
+	free(m_ptr);
+	*img_ptr = 0x01020304;
+	img_ptr ++;
+	*img_ptr = 0x05060708;
+	printf("img_ptr = 0x%x\n",img_ptr);
+	//*_gpio = 0x010000;
 }

13dbe6021a4830e0d92698f65cb62148753b8b6a
Edward Vidal Jr <develone@sbcglobal.net>
2018-11-27 18:50:22 -0700

    unpacking rgb to r g b . img_ptr = 0x20093c0 . buf_red = 0x20193c8 . buf_gr = 0x204c4e0 . buf_bl = 0x206c4e8 . Start of JPEG DWT! . w = 0x100 h = 0x100 . img_ptr = 0x20093c0 *img_ptr = 0xe22247c . img_ptr = 0x20093c4 *img_ptr = 0xde22083 . img_ptr = 0x20093c8 *img_ptr = 0xe221475 . img_ptr = 0x20093cc *img_ptr = 0xe32207b . img_ptr = 0x20093d0 *img_ptr = 0xe12287a . 0 0xe2 . 1 0xde . 2 0xe2 . 3 0xe3 . 4 0xe1 . 0 0x224 . 1 0x220 . 2 0x214 . 3 0x220 . 4 0x228 . 0 0x228 . 1 0x228 . 2 0x228 . 3 0x228 . 4 0x228

--- sw/board/jpeg.c
+++ sw/board/jpeg.c
@@ -21,25 +21,31 @@ for(i=0; i<256*256*12; i++) {
 int main(int argc, char **argv) {
 	
 	 
-	int *m_ptr,*img_ptr;
-	
+	int *m_ptr;
+	int *img_ptr;
+	int *buf_red, *buf_gr, *buf_bl;
+	int u,x,y,z;
 		
 
 	int val,i;
 	int w,h;
 	w = 256;
 	h = 256;
-	//malloc is for 3 * w*h 
-	printf("malloc should be 0x30000 196608\n");
-	printf("size of malloc 0x%x \n",w*h*3);
-	img_ptr = (int *)malloc(w*h*3);
+
+	img_ptr = (int *)malloc(w*h);
+	buf_red = (int *)malloc(w*h*2);
+	buf_gr = (int *)malloc(w*h*2);
+	buf_bl = (int *)malloc(w*h*2);
 	
 	//saving the img_ptr
-	m_ptr = img_ptr; 
+	m_ptr = img_ptr;
 	//*_gpio = GPIO_LEDR_SET;
 	//printf("gpio 0x%x\n",_gpio);
 	printf("img_ptr = 0x%x\n",img_ptr);
- 
+    printf("buf_red = 0x%x\n",buf_red);
+    printf("buf_gr = 0x%x\n",buf_gr);
+    printf("buf_bl = 0x%x\n",buf_bl);
+    //printf("top_of_data = 0x%x\n",top_of_data);
 	printf("Start of JPEG DWT!\n");
 	 
 	 
@@ -53,8 +59,11 @@ int main(int argc, char **argv) {
 	//zip_clear_sdram(img_ptr);
 	//printf("done clearing buf_ptr + 786432 \n");
 	//printf("free img_ptr !\n");	
-	 
+	//const int bb = 0x1ff;
+    //const int gg = 0x7fc00;		shift right 8
+    //const int rr = 0x1ff00000; 	shift right 20
 	printf("w = 0x%x h = 0x%x\n",w,h); 
+	
 	for(i=0;i<10;i++) {
 		printf("img_ptr = 0x%x *img_ptr = 0x%x\n",img_ptr,*img_ptr);
 		img_ptr ++;
@@ -69,8 +78,38 @@ int main(int argc, char **argv) {
 		printf("img_ptr = 0x%x *img_ptr = 0x%x\n",img_ptr,*img_ptr);
 		img_ptr ++;
 	}
-	printf("m_ptr = 0x%x\n",m_ptr);	
+	printf("m_ptr = 0x%x\n",m_ptr);
+	img_ptr = m_ptr;	
+	for(i=0;i<5;i++) {
+		x = *img_ptr;
+		y = 0x1ff00000;
+		z = x & y;
+		u = z>>20;
+		img_ptr++;
+		printf("%d 0x%x\n",i,u);
+	}
+	img_ptr = m_ptr;
+	for(i=0;i<5;i++) {
+		x = *img_ptr;
+		y = 0x7fc00;
+		z = x & y;
+		u = z>>8;
+		img_ptr++;
+		printf("%d 0x%x\n",i,u);
+	}
+	img_ptr = m_ptr;	
+	for(i=0;i<5;i++) {
+		
+		x = *img_ptr;
+		y = 0x1ff;
+		z = x & y;
+		
+		printf("%d 0x%x\n",i,u);
+	}	
 	//lifting(w,wptr,alt,fwd_inv);
 	free(img_ptr);
+	free(buf_red);
+	free(buf_gr);
+	free(buf_bl);
 	//*_gpio = 0x010000;
 }

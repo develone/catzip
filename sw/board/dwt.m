3863a011a488b759e86b9faef72c382ab0c61cde
Edward Vidal Jr <develone@sbcglobal.net>
2019-04-01 04:53:05 -0600

    missing files

--- sw/board/dwt.m
+++
@@ -1,7 +0,0 @@
-fid = fopen('dwt.bin','r'); im4 = fread(fid, [256,inf], 'int32'); fclose(fid);
-figure;
-imagesc(im4)
-colorbar
-%title "Red 1 level decomposition dwt.bin RPi  12/3/18"
-%title "Red 2 level decomposition dwt.bin RPi  12/3/18"
-title "blu 3 level decomposition dwt.bin RPi  12/3/18"
--- sw/board/dwt8.m
+++
@@ -1,4 +0,0 @@
-clear
-fid = fopen('dwt8.bin','r'); im7= fread(fid, [256,inf], 'char'); fclose(fid);figure;
-imagesc(im7);
-colorbar;

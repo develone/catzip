8cb6e2269fcc4f31855f8dce6279dde113b3d717
Ed Vidal Jr <devel@mypi3-16.attlocal.net>
2021-04-06 19:30:09 -0600

    -                       wb_sdram_cyc, wb_sdram_stb, wb_sdram_we, +                       wb_sdram_cyc, +                       /* verilator lint_off WIDTH */ +                       ((wb_sdram_stb)&&(wb_sdram_sel)), +                       /* verilator lint_off WIDTH */ +                       wb_sdram_we,                         wb_sdram_addr[(@$LGMEMSZ-1):0],                         wb_sdram_data, // 32 bits wide                         wb_sdram_sel,  // 32/8 bits ./arm-wbregs 16777216 0x55 ./arm-wbregs 16777220 0xaa ./arm-wbregs 16777224 0x01 ./arm-wbregs 16777228 0x02 ./arm-wbregs 16777232 0x00010000 ./arm-wbregs 16777236 0x00020000 ./arm-wbregs 16777216 ./arm-wbregs 16777220 ./arm-wbregs 16777224 ./arm-wbregs 16777228 ./arm-wbregs 16777232 ./arm-wbregs 16777236

--- sw/host/wr.sh
+++ sw/host/wr.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 ./arm-wbregs 16777216 0x55
 ./arm-wbregs 16777220 0xaa
+./arm-wbregs 16777224 0x01
+./arm-wbregs 16777228 0x02
+./arm-wbregs 16777232 0x00010000
+./arm-wbregs 16777236 0x00020000
 ./arm-wbregs 16777216
 ./arm-wbregs 16777220
+./arm-wbregs 16777224
+./arm-wbregs 16777228
+./arm-wbregs 16777232
+./arm-wbregs 16777236
--- sw/host/wr_test.sh
+++ sw/host/wr_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
+#10485760	BLKRAM
+#16777216	SDRAM
          COUNTER=2000
-	ADDRESS=33554428
+	ADDRESS=16777216
          until [  $COUNTER -lt 1 ]; do
              echo COUNTER $COUNTER
              let COUNTER-=1

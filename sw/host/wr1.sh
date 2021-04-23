ac87fa90f9e3345306b2be019d67ff85ea44a364
Ed Vidal Jr <devel@mypi3-16.attlocal.net>
2021-04-23 13:58:39 -0600

    Testing bkram in simulation devel@mypi3-16:~/testbuilds/catzip-tst/catzip/sw/host $ ./wr1.sh 01400000 (     RAM)-> 00000055 01400004 (        )-> 000000aa 01400008 (        )-> 00000001 0140000c (        )-> 00000002 01400010 (        )-> 00010000 01400014 (        )-> 00020000 01400000 (     RAM) : [...U] 00000055 01400004 (        ) : [....] 000000aa 01400008 (        ) : [....] 00000001 0140000c (        ) : [....] 00000002 01400010 (        ) : [....] 00010000 01400014 (        ) : [....] 00020000 01400004 83 20 E2 0D 01400004 (        )-> 8320e20d 01040008 75 14 22 0E 01400008 (        )-> 7514220e 014002f4 6A 08 C2 0D 014002f4 (        )-> 6a08c20d 014006f0 65 F0 F1 0D 014006f0 (        )-> 65f0f10d 01400a70 5D 58 41 0B 01400a70 (        )-> 5d58410b 00000100   5E 88 D1 0C 01400100 (        )-> 6284010d 00001000   67 00 02 0E 01401000 (        )-> 6700020e 000010F0   58 74 B1 0C 014011f0 (        )-> 5874b10d 000011Fc 61 8C D1 0C 014010fc (        )-> 618cd10c 01400004 (        ) : [....] 8320e20d 01400008 (        ) : [u.".] 7514220e 014002f4 (        ) : [j...] 6a08c20d 014006f0 (        ) : [e...] 65f0f10d 01400a70 (        ) : []XA.] 5d58410b 01400100 (        ) : [b...] 6284010d 01401000 (        ) : [g...] 6700020e 014011f0 (        ) : [Xt..] 5874b10d 014010fc (        ) : [a...] 618cd10c

--- sw/host/wr1.sh
+++ sw/host/wr1.sh
@@ -12,3 +12,40 @@
 ./arm-wbregs 20971536 
 ./arm-wbregs 20971540
 
+echo '01400004 83 20 E2 0D'
+./arm-wbregs 0x01400004 0x8320e20d
+
+echo '01040008 75 14 22 0E'
+./arm-wbregs 0x01400008 0x7514220e
+
+echo '014002f4 6A 08 C2 0D'
+./arm-wbregs 0x014002f4 0x6a08c20d
+
+echo '014006f0 65 F0 F1 0D'
+./arm-wbregs 0x014006f0 0x65f0f10d
+
+echo '01400a70 5D 58 41 0B'
+./arm-wbregs 0x01400a70 0x5d58410b
+
+echo '00000100   5E 88 D1 0C' 
+./arm-wbregs 0x01400100 0x6284010d
+
+echo '00001000   67 00 02 0E' 
+./arm-wbregs 0x01401000 0x6700020e
+
+echo '000010F0   58 74 B1 0C' 
+./arm-wbregs 0x014011f0 0x5874b10d
+
+
+echo '000011Fc 61 8C D1 0C'
+./arm-wbregs 0x014010fc 0x618cd10c
+
+./arm-wbregs 0x01400004
+./arm-wbregs 0x01400008
+./arm-wbregs 0x014002f4
+./arm-wbregs 0x014006f0
+./arm-wbregs 0x01400a70
+./arm-wbregs 0x01400100
+./arm-wbregs 0x01401000
+./arm-wbregs 0x014011f0
+./arm-wbregs 0x014010fc
